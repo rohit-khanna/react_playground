71cdcf79d0f395c1c426e969cd2d7da29b3dc444
Rohit Khanna <therohitkhanna@live.com>
2024-02-13 10:42:37 -0500

    Revert "Revert "Feature/loadmore functionality- when API doesnot have a Pagination support""

--- playground/src/index.tsx
+++ playground/src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import React from "react";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import Router from "./RouterConfig";
@@ -9,10 +10,12 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
+  <React.StrictMode>
   <div className="container">
     <RouterProvider router={Router} />
-  </div>
+  </div></React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
